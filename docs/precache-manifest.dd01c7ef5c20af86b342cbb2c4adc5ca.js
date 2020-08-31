8f092f4a4e875c0889c4297b7a0b9552be99c780
Stiwart Antunez Gamboa <stiwart.antunez@gmail.com>
2020-08-30 22:02:49 -0400

    docs added

---
+++ docs/precache-manifest.dd01c7ef5c20af86b342cbb2c4adc5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dc838a7681e38dbf4bbdd0cd93f825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c063b6846ff589225d",
+    "url": "/static/css/main.fe5a4e94.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c063b6846ff589225d",
+    "url": "/static/js/main.2ba20707.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
